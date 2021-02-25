b865bb5b2f43025e59c0ad5e5da0cfb6c0acafb5
Quincy Cheng <quincy.cheng@gmail.com>
2021-02-25 19:01:52 +0800

    Switching URL from http://conjur to https://proxy

--- bin/02-build-java-app.sh
+++ bin/02-build-java-app.sh
@@ -5,10 +5,9 @@ docker-compose build javaapp
 
 echo "Saving settings for App as .env file"
 cat > .env << EOF
-CONJUR_APPLIANCE_URL=http://conjur
+CONJUR_APPLIANCE_URL=https://proxy
 CONJUR_ACCOUNT=default
 CONJUR_AUTHN_API_KEY=$(tail -n +2 tmp/my_app_data| jq '.created_roles."default:host:JavaApp/myJavaApp".api_key' -r)
 VARIABLE_ID=JavaApp/secretVar
 EOF
 
-#CONJUR_AUTHN_LOGIN=$(tail -n +2 tmp/my_app_data| jq '.created_roles."default:host:JavaApp/myJavaApp".id' -r)
